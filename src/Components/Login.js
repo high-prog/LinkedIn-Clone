0cd03c9cedbfcbe1d4ed46cfa05abeecb33ee897
high-prog <58605707+high-prog@users.noreply.github.com>
2023-01-11 21:43:02 +0530

    join Now button formed

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -8,10 +8,35 @@ const Login = (props) => {
         <a href="/">
           <img src="/images/login-logo.svg" alt="" />
         </a>
+        <div>
+          <Join>Join Now</Join>
+          <Signin>Sign In</Signin>
+        </div>
       </Nav>
     </Container>
   );
 };
+
+
+const Join = styled.a`
+  font-size:16px;
+  padding: 10px 12px;
+  text-decoration:none;
+  border-radius:4px;
+  color:rgba(0,0,0,0.6)
+  margin-right:12px;
+
+  &:hover{
+    background-color:rgba(0,0,0,0.09);
+    color:#0a66c2;
+    border-radius:24px;
+    text-decoration:none;
+  }
+`;
+
+const Signin = styled.a`
+
+`;
 
 
 const Container = styled.div`
