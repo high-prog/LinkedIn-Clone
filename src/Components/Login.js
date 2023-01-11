36679536f7ceeebda3e0e52e7e2cb78041f43e6a
high-prog <58605707+high-prog@users.noreply.github.com>
2023-01-11 22:20:05 +0530

    Added Hero to Login Page

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -13,9 +13,76 @@ const Login = (props) => {
           <Signin>Sign In</Signin>
         </div>
       </Nav>
+      <Section>
+          <Hero>
+            <h1>Welcome to your Professional Community</h1>
+            <img src="/images/login-hero.svg" alt="LoginHero" />
+          </Hero>
+      </Section>
     </Container>
   );
 };
+
+
+const Hero = styled.div`
+width:100%;
+
+h1 {
+  padding-bottom:0px;
+  font-size:70px;
+  
+  color:#2977c9;
+  font-weight:200;
+  line-height:70px;
+  
+  @media (max-width: 768px){
+    flex-direction:column;
+    text-align:center;
+    font-size:20px;
+    width:100%;
+    line-height:2;
+  }
+}
+
+img {
+  
+    z-index:-1;
+    position:absolute;
+    width: 700px;
+    height:670px;
+    bottom:-2px;
+    right: -150px;
+    @media (max-width: 768px){
+      top:230px;
+      width: initial;
+      position: initial;
+      height:initial;
+    }
+}
+`;
+
+
+
+const Section = styled.section`
+  didplay:flex;
+  align-content:start;
+  min-height:700px;
+  padding-bottom:138px;
+  padding-top:40px;
+  
+  position:relative;
+  flex-wrap:wrap;
+  width: 100%;
+  max-width:1128px;
+  align-items:center;
+  margin:auto;
+
+
+  @media (max-width: 768px){
+    margin:auto;
+    min-height:0px;
+  }
+`;
 
 
 const Join = styled.a`
@@ -25,6 +92,7 @@ const Join = styled.a`
   border-radius:4px;
   color:rgba(0,0,0,0.6)
   margin-right:12px;
+  cursor:pointer;
 
   &:hover{
     background-color:rgba(0,0,0,0.09);
@@ -45,6 +113,7 @@ const Signin = styled.a`
   line-height:40px;
   text-align:center;
   backgroun-color:rgba(0,0,0,0);
+  cursor:pointer;
   
   &:hover{
   background-color:rgba(112,181,249,0.15);
