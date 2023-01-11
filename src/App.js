772b95865bbc0381ca0c5c8c84fb69b659e19280
high-prog <58605707+high-prog@users.noreply.github.com>
2023-01-11 23:15:11 +0530

    Starting Header

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import './style.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Components/Login.js';
-import {BrowserRouter as Router , Routes , Route} from 'react-router-dom';
+import Header from './Components/Header.js';
+import Home from './Components/Home.js';
 
- function App() {
+function App() {
   return (
-
-<>
-    <div className="App">
-            <Router>
-            <Routes>
+    <>
+      <div className="App">
+        <Router>
+          <Routes>
             <Route path="/" element={<Login />} />
-            </Routes>
-            </Router>
-    </div>
-</>
+            <Route
+              path="/home"
+              element={
+                <>
+                  <Header />
+                  <Home />
+                </>
+              }
+            />
+          </Routes>
+        </Router>
+      </div>
+    </>
   );
 }
 
