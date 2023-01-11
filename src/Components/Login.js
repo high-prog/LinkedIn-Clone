48726a810e6ae89a04879429ccadbee7216c1f07
high-prog <58605707+high-prog@users.noreply.github.com>
2023-01-11 19:47:54 +0530

    1 file modified

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -14,6 +14,28 @@ const Login = (props) => {
 };
 
 
-const Container = styled.div``;
-const Nav = styled.nav``;
+const Container = styled.div`
+  padding: 0px;
+
+`;
+const Nav = styled.nav`
+max-width:1128px;
+margin:auto;
+padding:12px 0 16px;
+display:flex;
+position:relative;
+justify-content:space-between;
+flex-wrap: nowrap;
+
+& > a {
+  width:135px;
+  height:34px;
+}
+
+@media (max-width:768px){
+  padding: 10px 5px;
+}
+
+
+`;
 export default Login;
