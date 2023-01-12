7b2fb7848b8ca392a778f4079ca05da1fd1921c0
high-prog <58605707+high-prog@users.noreply.github.com>
2023-01-12 15:33:47 +0530

    rightside started

--- src/Components/Rightside.js
+++ src/Components/Rightside.js
@@ -1,17 +1,85 @@
-import styled from 'styled-components'
-import React from 'react'
+import styled from 'styled-components';
+import React from 'react';
 
 const Rightside = (props) => {
-  return(
+  return (
     <Container>
-    rightside
+      <FollowCard>
+        <Title>
+          <h2>Add to your feed</h2>
+          <img src="/images/feed-icon.svg" alt="" />
+        </Title>
+        <FeedList>
+          <li>
+            <a>
+              <Avatar />
+            </a>
+            <div>
+              <span>#LinkedIn</span>
+              <button>Follow</button>
+            </div>
+          </li>
+          <li>
+             <a >
+               <Avatar />
+             </a>
+             <div>
+               <span>#Video</span>
+               <button>Follow</button>
+             </div>
+          </li>
+        </FeedList>
+      </FollowCard>
     </Container>
-  )
-}
+  );
+};
 
 const Container = styled.div`
   grid-area:rightside;
   
 `;
+
+const FollowCard = styled.div`
+text-align:center;
+overflow:hidden;
+margin-bottom:8px;
+background-color:#fff;
+border-radius:5px;
+box-shadow: 0 0 0 1px rgba(0 0 0/ 15%), 0 0 0 rgb(0 0 0/ 20%);
+border:none;
+position:relative;
+padding:12px;
+`;
+
+const Title = styled.div`
+display: inline-flex;
+align-items:center;
+justify-content:space-between;
+font-size:16px;
+width:100%;
+color: rgba(0,0,0,0.6); 
+`;
+
+
+
+const FeedList = styled.div`
+margin-top:16px;
+display:flex;
+align-items:center;
+`;
+const Avatar = styled.div``;
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 export default Rightside;
