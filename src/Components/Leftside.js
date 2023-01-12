eeac75059efb02952bebd74e5fd98b8c3bc96269
high-prog <58605707+high-prog@users.noreply.github.com>
2023-01-12 14:37:26 +0530

    Card halfway

--- src/Components/Leftside.js
+++ src/Components/Leftside.js
@@ -6,48 +6,19 @@ const Leftside = (props) => {
     <Container>
       <ArtCard>
         <UserInfo>
-          <CardBackground>
-            <a >
-              <Photo />
-              <Link>Welcome, there!</Link>
-            </a>
-            <a>
-              <AddPhotoText>Add Photo</AddPhotoText>
-            </a>
-          </CardBackground>
+          <CardBackground />
+          <a>
+            <Photo />
+            <Link>Welcome, there!</Link>
+          </a>
+          <a>
+            <AddPhotoText>Add Photo</AddPhotoText>
+          </a>
         </UserInfo>
       </ArtCard>
     </Container>
   );
 };
-
-const UserInfo = styled.div`
-  border-bottom:1px solid rgba(0,0,0,0.15);
-  padding:12px 12px 16px;
-  word-wrap:break-word;
-  word-break:break-word;
-
-`;
-
-
-
-const CardBackground = styled.div`
-  background: url("/images/card-bg.svg");
-  background-position: center;
-  background-size:462px;
-  height:54px;
-  margin:-12px -12px 0; 
-`;
-const Photo = styled.div`
-  box-shadow: none;
-  background-image: url("/images/photo.svg");
-`;
-
-
-
-const Link = styled.div``;
-const AddPhotoText = styled.div``;
-
 const Container = styled.div`
   grid-area:leftside;
 
@@ -65,5 +36,39 @@ border:none;
 box-shadow: 0 0 0 1px rgba(0 0 0/ 15%), 0 0 0 rgb(0 0 0/ 20%);
 
 `;
+
+const UserInfo = styled.div`
+  border-bottom:1px solid rgba(0,0,0,0.15);
+  padding:12px 12px 16px;
+  word-wrap:break-word;
+  word-break:break-word;
+  
+`;
+
+const CardBackground = styled.div`
+  background: url("/images/card-bg.svg");
+  background-position: center;
+  background-size: 462px;
+  margin: -12px -12px 0; 
+  height:54px;
+`;
+const Photo = styled.div`
+  box-shadow: none;
+  background-image: url("/images/photo.svg");
+  width:72px;
+  height:72px; 
+  box-sizing:border-box;
+  background-clip:content-box;
+  background-color:white;
+  background-position:center;
+  background-size:80%;
+  background-repeat:no-repeat;
+  border: 2px solid white;
+  margin: -38px auto 5px;
+  border-radius:10%;
+`;
+
+const Link = styled.div``;
+const AddPhotoText = styled.div``;
 
 export default Leftside;
